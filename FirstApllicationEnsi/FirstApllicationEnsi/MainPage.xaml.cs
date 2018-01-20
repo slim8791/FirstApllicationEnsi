ca2ea5612880313b27051fb10c8c536f9e3cfaa6
Slim HAMMAMI <slim.hammami.ext@easytek.tn>
2018-01-20 18:06:29 +0100

    Add project files.

---
+++ FirstApllicationEnsi/FirstApllicationEnsi/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApllicationEnsi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           InitializeComponent();
+            MySlider.Value = 0.1;
+        }
+        //
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+
+            DisplayAlert("This a message", "Welcome to Xamarin Forms", "Cancel");
+        }
+
+        //private void Slider_OnValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    MyBoxView.Opacity = e.NewValue;
+        //    MyLabel.Text = String.Format("Value is {0:F2}", e.NewValue);
+        //}
+    }
+}
---
+++ FirstApllicationEnsi/FirstApllicationEnsi/PhoneKeyboard.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApllicationEnsi
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PhoneKeyboard : ContentPage
+    {
+        public PhoneKeyboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Button myBtn = (Button) sender;
+            Number.Text = Number.Text + myBtn.Text;
+        }
+    }
+}
